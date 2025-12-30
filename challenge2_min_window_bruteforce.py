8cccd58281ffa03cb385c2782e7f2785ee1ce3ab
hafsa <ayanhafsa41@gmail.com>
2025-12-30 22:46:15 +0500

    Create challenge2_min_window_bruteforce.py

---
+++ challenge2_min_window_bruteforce.py
@@ -0,0 +1,10 @@
+string=input("Enter the log: ")
+lengt=input("Enter the pattern: ")
+for i in range(string):
+   for j in range(lengt):
+      #substring=string[i:j+1]
+req={}
+if char in lengt:
+   req+=1
+else:
+    req=1
