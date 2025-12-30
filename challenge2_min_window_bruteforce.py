d24a134f7487e90e7d8cbd128964425e0fc4a8c4
hafsa <ayanhafsa41@gmail.com>
2025-12-30 21:30:04 +0500

    Update challenge2_min_window_bruteforce.py

--- challenge2_min_window_bruteforce.py
+++ challenge2_min_window_bruteforce.py
@@ -1,1 +1,18 @@
+#practice 
+
+n=int(input("Enter the no of team members: "))
+contribution=[]
+for i in range(n):
+    x=input("Enter contribution; ")
+    contribution.append(x)
+print(contribution)
+impact=[]
+for i in range(n):
+    for j in range(n):
+       y= x*contribution
+       impact.append(y)
+print(impact)
+
+
+
 
